1246033dcbd46e3b50abf37664a7c1784fe62d62
Raja-vemana <84390320+Raja-vemana@users.noreply.github.com>
2021-06-21 12:14:17 +0530

    Add files via upload

---
+++ endswith().py
@@ -0,0 +1,15 @@
+s='python'
+res=s.endswith('n')
+print (res)
+res=s.endswith('N')
+print (res)
+res=s.endswith('n',0,6)
+print (res)
+res=s.endswith('n',1,5)
+print (res)
+
+...
+True
+False
+True
+False
