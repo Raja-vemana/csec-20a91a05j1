84a5170cd0548a9fa099a641154ba0e8a991893e
Raja-vemana <84390320+Raja-vemana@users.noreply.github.com>
2021-06-25 11:57:27 +0530

    Add files via upload

---
+++ alternate strings.py
@@ -0,0 +1,16 @@
+print('enter two strings of equal length')
+a=input('enter first string')
+b=input('enter second string')
+if len(a)==len(b):
+    for i in range(len(a)):
+        print(a[i],b[i],end=" ")
+               
+else:
+    print('Both the strings are not having equal length')
+
+
+#enter two strings of equal length
+#enter first stringraja
+#enter second stringrama
+#r r a a j m a a 
+
