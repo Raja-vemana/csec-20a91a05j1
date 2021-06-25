3c9cd2eebeab0df70e12a5d679379a3d3a95fdd7
Raja-vemana <84390320+Raja-vemana@users.noreply.github.com>
2021-06-25 12:09:01 +0530

    Add files via upload

---
+++ vowels in string.py
@@ -0,0 +1,14 @@
+s=input()
+res=s.find('a' or 'e' or 'i' or 'o' or 'u' or 'A' or 'E' or 'I' or 'O' or 'U')
+if res==-1:
+     print("vowels are absent")
+else:
+     print("vowels are present")
+
+
+
+#rr
+#vowels are absent
+#raja
+#vowels are present
+
